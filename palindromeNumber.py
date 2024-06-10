2679ace042b2bb94f1e255780e22b02cf064afb5
ANDREA CALDERON <54920275+imandrec@users.noreply.github.com>
2024-06-09 21:46:11 -0400

    Update palindromeNumber.py

--- palindromeNumber.py
+++ palindromeNumber.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def isPalindrome(self, x):
         nums = str(x)
-        y = nums[::-1]
+        y = nums[::-1] #Reverse the list
         if nums == y:
             return True
         else:
